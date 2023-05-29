3197ea9b3c82392de9f2591dd512c1a0b1f19f1b
Uros Pocek <uros.pocek@gmail.com>
2023-05-29 11:14:43 +0200

    feat: Unique columns added

--- travel_app/travel_app/MVVM/Model/RestaurantTypes.cs
+++ travel_app/travel_app/MVVM/Model/RestaurantTypes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace travel_app.MVVM.Model
         [Key]
         public int Id { get; set; }
         [Required]
+        [Index(IsUnique = true)]
         public string Name { get; set; }
 
     }
