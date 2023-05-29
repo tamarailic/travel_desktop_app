63a730ee5bf8d201d3140688e0cde3c3c1335213
Tamara Ilic <tamarailic11@gmail.com>
2023-05-29 20:01:56 +0200

    feat:Responsive user UI

---
+++ travel_app/travel_app/MVVM/View/UserDetailsView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace travel_app.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for UserDetailsView.xaml
+    /// </summary>
+    public partial class UserDetailsView : UserControl
+    {
+        public UserDetailsView()
+        {
+            InitializeComponent();
+        }
+
+        private void Reserve(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Želite da rezervišete ovo putovanje?", "Rezervacija", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Uspešna rezervacija.", "Rezervacija", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Rezervacija odbijena.", "Rezervacija", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
