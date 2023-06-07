f365a381caad6da94075397351c6b4d53ca71798
Tamara Ilic <tamarailic11@gmail.com>
2023-06-07 19:05:30 +0200

    fix: Edit validation

--- travel_app/travel_app/MVVM/View/EditTravelView.xaml.cs
+++ travel_app/travel_app/MVVM/View/EditTravelView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using travel_app.MVVM.ViewModel;
 using WPFCustomMessageBox;
 
 namespace travel_app.MVVM.View
@@ -21,84 +22,11 @@ namespace travel_app.MVVM.View
     /// </summary>
     public partial class EditTravelView : UserControl
     {
-        public string TravelsName { get; set; }
-        public string From { get; set; }
-        public string To { get; set; }
-        public string DescLong { get; set; }
-        public string DescShort { get; set; }
-        public string Price { get; set; }
+       
         public EditTravelView()
         {
             InitializeComponent();
         }
 
-        private bool ValidateData()
-        {
-            var bindingExpressionTravelName = TravelName.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionTravelName.UpdateSource();
-            if (Validation.GetHasError(TravelName))
-            {
-                var errors = Validation.GetErrors(TravelName);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška","U red");
-                return false;
-            }
-
-            var bindingExpressionStartLocation = StartLocation.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionStartLocation.UpdateSource();
-            if (Validation.GetHasError(StartLocation))
-            {
-                var errors = Validation.GetErrors(StartLocation);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U redu");
-                return false;
-            }
-
-            var bindingExpressionEndLocation = EndLocation.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionEndLocation.UpdateSource();
-            if (Validation.GetHasError(EndLocation))
-            {
-                var errors = Validation.GetErrors(EndLocation);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U redu");
-                return false;
-            }
-
-            var bindingExpressionTravelShortDescription = TravelShortDescription.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionTravelShortDescription.UpdateSource();
-            if (Validation.GetHasError(TravelShortDescription))
-            {
-                var errors = Validation.GetErrors(TravelShortDescription);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U red");
-                return false;
-            }
-
-            var bindingExpressionTravelLongDescription = TravelDescription.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionTravelLongDescription.UpdateSource();
-            if (Validation.GetHasError(TravelDescription))
-            {
-                var errors = Validation.GetErrors(TravelDescription);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U red");
-                return false;
-            }
-
-            var bindingExpressionTravelPrice = TravelPrice.GetBindingExpression(TextBox.TextProperty);
-            bindingExpressionTravelPrice.UpdateSource();
-            if (Validation.GetHasError(TravelPrice))
-            {
-                var errors = Validation.GetErrors(TravelPrice);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U red");
-                return false;
-            }
-
-            var bindingExpressionTravelDate = TravelDate.GetBindingExpression(DatePicker.SelectedDateProperty);
-            bindingExpressionTravelDate.UpdateSource();
-            if (Validation.GetHasError(TravelDate))
-            {
-                var errors = Validation.GetErrors(TravelDate);
-                CustomMessageBox.ShowOK(errors[0].ErrorContent.ToString(), "Greška", "U red");
-                return false;
-            }
-
-            return true;
-
-        }
     }
 }
